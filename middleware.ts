99358c54192525a06dbf5e4552e08c427acbe237
Harshit Soni <harshit@ailusion.in>
2025-12-16 16:15:01 +0530

    do testing

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,38 @@
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// export function middleware(req: NextRequest) {
+//   const publicPaths = ["/login", "/api/auth"];
+//   if (publicPaths.some(path => req.nextUrl.pathname.startsWith(path))) {
+//     return NextResponse.next();
+//   }
+
+//   const cookieToken = req.cookies.get("next-auth.session-token")?.value;
+//   const secureCookieToken = req.cookies.get("__Secure-next-auth.session-token")?.value;
+//   const sessionToken = cookieToken || secureCookieToken;
+
+//   console.log("Session Token",sessionToken)
+//   console.log("cookie Token",cookieToken)
+
+//   if (req.nextUrl.pathname.startsWith("/dashboard") && !sessionToken) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+
+//   console.log("Next Response");
+  
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/dashboard/:path*"],
+// };
+
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const publicPaths = ["/login", "/api/auth"];
-  if (publicPaths.some(path => req.nextUrl.pathname.startsWith(path))) {
-    return NextResponse.next();
-  }
-
-  const cookieToken = req.cookies.get("next-auth.session-token")?.value;
-  const secureCookieToken = req.cookies.get("__Secure-next-auth.session-token")?.value;
-  const sessionToken = cookieToken || secureCookieToken;
-
-  console.log("Session Token",sessionToken)
-  console.log("cookie Token",cookieToken)
-
-  if (req.nextUrl.pathname.startsWith("/dashboard") && !sessionToken) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  console.log("Next Response");
-  
-
-  return NextResponse.next();
+  console.log("🔥 MIDDLEWARE RUNNING:", req.nextUrl.pathname);
+  return NextResponse.redirect(new URL("/login", req.url));
 }
-
-export const config = {
-  matcher: ["/dashboard/:path*"],
-};
